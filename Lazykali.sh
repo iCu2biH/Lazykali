a0057c3d9009a18ab471d156384beee361ea80ab
Rupert Edwards <ruped24@gmail.com>
2015-07-07 18:41:59 -0400

    Update Lazykali.sh

--- Lazykali.sh
+++ Lazykali.sh
@@ -49,7 +49,7 @@ if [[ "$silent" = "1" ]];then
 	echo "Not checking for a new version : silent mode."
 else
 	changelog=$(curl --silent -q http://yourgeekonthego.com/scripts/lazykali/changelog)
-	last_version=$(curl --silent -q http://yourgeekonthego.com/scripts/lazykali/version) #store last version number to variable
+	last_version=$(curl -q  --silent https://github.com/ruped24/Lazykali/blob/master/version |grep -m2 "js-file-line" |grep "20130525" |cut -c 69-76) #store last version number to variable
 	if [[ $last_version > $version ]];then # Comparing to current version
 		echo -e "You are running version \033[31m$version\033[m, do you want to update to \033[32m$last_version\033[m? (Y/N)
 Last changes are :
