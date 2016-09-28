ff94fb7d1e3d1d3dc787a1c1dbb0341efb9f3f20
Rupert Edwards <ruped24@gmail.com>
2016-09-28 12:24:07 +0000

    [Fixed] exploitdb

--- Lazykali.sh
+++ Lazykali.sh
@@ -1091,15 +1091,11 @@ if [[ $install = Y || $install = y ]] ; then
 ### Update Exploitdb
 function updateexploitdb {
 	echo -e "\033[31mThis script will update your Exploitdb\033[m"
-	cd /usr/share/exploitdb
-	rm -rf platforms files.csv
-	wget --no-check-certificate http://www.exploit-db.com/archive.tar.bz2
-	mv archive.tar.bz2 archive.zip
-	unzip archive.zip
-	rm -rf archive.zip
-	mv exploit-database-master platforms
-	mv platforms/files.csv ./
-	echo -e "\e[32m[-] Done Updating Exploitdb!\e[0m"	
+	cd /usr/share/exploitdb && rm -rf *
+	git clone https://github.com/offensive-security/exploit-database && \
+	cd /usr/share/exploitdb/exploit-database/platforms && ln -s ../platforms ./
+	ln -s /usr/share/exploitdb/exploit-database/files.csv /usr/share/exploitdb/files.csv
+	echo -e "\e[32m[-] Done Updating Exploitdb!\e[0m"
 }
 
 #### Searchsploit
